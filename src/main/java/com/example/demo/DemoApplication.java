4b0900a0c1f60ea03cb9e6a236bcc0126e65ea5d
luongtheanhtk <luongtheanhtk12@gmail.com>
2022-10-24 11:37:55 +0700

    thay luongtheanh

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
 
-        System.out.println("nguyen cong thanh 966");
+        System.out.println("luongtheanh");
         SpringApplication.run(DemoApplication.class, args);
     }
 
