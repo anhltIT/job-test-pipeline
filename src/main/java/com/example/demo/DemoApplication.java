00e6e59af2362e08bc1d62fa38e809df38cad51a
luongtheanhtk <luongtheanhtk12@gmail.com>
2022-10-26 15:11:10 +0700

    them 96

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
 
-        System.out.println("luongtheanh");
+        System.out.println("luongtheanh96");
         SpringApplication.run(DemoApplication.class, args);
     }
 
